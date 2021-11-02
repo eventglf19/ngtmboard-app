2a5d5cf2ff45eb4d5652fb58bdf1ed0fa4f9a0f7
eventglf19 <84870547+eventglf19@users.noreply.github.com>
2021-11-02 22:09:13 +0300

    Add files via upload

--- infoTest.php
+++ infoTest.php
@@ -4,13 +4,12 @@
 
 <?php
 
-	$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-	$txt = "John Doe\n";
-	fwrite($myfile, $txt);
-	$txt = "Jane Doe\n";
+	$myfile = fopen("ngtmQuizLogs.txt", "a") or die("Unable to open file!");
+	$txt = date("Y-m-d h:i:sa") + $_GET['q'] + $_GET['u'] + "\n";
 	fwrite($myfile, $txt);
 	fclose($myfile);
 
+	echo "done";
 ?>
 
 </body>
