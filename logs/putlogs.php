0d9e24e919cbc1716ef0c38ca0414c30d16b1660
eventglf19 <84870547+eventglf19@users.noreply.github.com>
2021-11-02 23:36:20 +0300

    Add files via upload

--- logs/putlogs.php
+++ logs/putlogs.php
@@ -1,10 +1,11 @@
 
 <?php
+	$myfilename = "ngtmQuizLogs" . date("Y-m") . ".txt";
 
-	$myfile = fopen("ngtmQuizLogs" . date("Y-m") . ".txt", "a+") or die("Unable to open file!");
+	$myfile = fopen($myfilename, "a+") or die("Unable to open file!");
 	$txt = date("Y-m-d h:i:sa"). "," . $_GET['q'] . "," . $_GET['u']  .  "\n";
 	fwrite($myfile, $txt);
 	fclose($myfile);
 
-	echo $myfile;
+	echo $myfilename;
 ?>
